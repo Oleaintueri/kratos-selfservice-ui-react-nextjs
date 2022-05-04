dabd9bfd7dc18f76aa6476253946729f93c0f971
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-05-04 16:12:33 +0200

    feat: add ory sdk url

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,6 +1,7 @@
 import { Configuration, V0alpha2Api } from '@ory/client'
 
 export default new V0alpha2Api(new Configuration({
+  basePath: process.env.ORY_SDK_URL,
   baseOptions: {
     withCredentials: true,
   }
