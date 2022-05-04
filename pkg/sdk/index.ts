b30b6b2e8ed0cf5f5daedc4a9b7fa46f148b2161
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-05-04 16:08:51 +0200

    fix: remove integrations and include credentials

--- pkg/sdk/index.ts
+++ pkg/sdk/index.ts
@@ -1,4 +1,7 @@
 import { Configuration, V0alpha2Api } from '@ory/client'
-import { edgeConfig } from '@ory/integrations/next'
 
-export default new V0alpha2Api(new Configuration(edgeConfig))
+export default new V0alpha2Api(new Configuration({
+  baseOptions: {
+    withCredentials: true,
+  }
+}))
